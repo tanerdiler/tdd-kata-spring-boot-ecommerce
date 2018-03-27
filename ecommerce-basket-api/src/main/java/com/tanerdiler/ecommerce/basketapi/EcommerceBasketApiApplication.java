b29a9f62b351c1a591d1830c1513a7c272d0faf4
taner <taner.diler@gmail.com>
2018-03-27 12:25:34 +0300

    Initial Commit for Api

---
+++ ecommerce-basket-api/src/main/java/com/tanerdiler/ecommerce/basketapi/EcommerceBasketApiApplication.java
@@ -0,0 +1,12 @@
+package com.tanerdiler.ecommerce.basketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBasketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBasketApiApplication.class, args);
+	}
+}
---
+++ ecommerce-basket-api/src/test/java/com/tanerdiler/ecommerce/basketapi/EcommerceBasketApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tanerdiler.ecommerce.basketapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcommerceBasketApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
